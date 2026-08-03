cfd604faaeaee34467566cdcbe2566a5e85c4fea
ttelcl <9429819+ttelcl@users.noreply.github.com>
2026-08-03 22:32:52 +0300

    Initial empty test application

---
+++ CellGrids.TestApp/App.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Windows;
+
+using CellGrids.TestApp.Main;
+
+using ControlzEx.Theming;
+
+namespace CellGrids.TestApp;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+
+  /// <summary>
+  /// Instead of using a Startup Uri, create the window manually.
+  /// This method is referenced in the header of app.xaml instead of
+  /// a startup URI.
+  /// </summary>
+  private void App_Startup(object sender, StartupEventArgs e)
+  {
+    DispatcherUnhandledException += (s, e) =>
+      ProcessUnhandledException(e);
+    Trace.TraceInformation($"App.App_Startup enter");
+    ThemeManager.Current.ChangeTheme(this, "Dark.Olive");
+    MainModel = new MainViewModel();
+    var mainWindow = new MainWindow() {
+      DataContext = MainModel,
+    };
+    Trace.TraceInformation($"App.App_Startup showing main window");
+    mainWindow.Show();
+    Trace.TraceInformation($"App.App_Startup done");
+  }
+
+  public MainViewModel? MainModel { get; private set; }
+
+  private void ProcessUnhandledException(
+    System.Windows.Threading.DispatcherUnhandledExceptionEventArgs evt)
+  {
+    var ex = evt.Exception;
+    Trace.TraceError($"Error: {ex}");
+    MessageBox.Show(
+      $"{ex.GetType().FullName}\n{ex.Message}",
+      "Error",
+      MessageBoxButton.OK,
+      MessageBoxImage.Error);
+    evt.Handled = MainWindow?.IsLoaded ?? false;
+  }
+
+  private void Application_Exit(object sender, ExitEventArgs e)
+  {
+    Trace.TraceInformation("Application_Exit: Cleanup");
+  }
+
+}
---
+++ CellGrids.TestApp/Main/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace CellGrids.TestApp.Main;
+
+public class MainViewModel: ObservableObject
+{
+  public MainViewModel()
+  {
+    ExitCommand = new RelayCommand(() => {
+      var w = Application.Current.MainWindow;
+      w?.Close();
+    });
+  }
+
+  public ICommand ExitCommand { get; }
+
+  public string StatusMessage {
+    get => _statusMessage;
+    set {
+      if(SetProperty(ref _statusMessage, value))
+      {
+      }
+    }
+  }
+  private string _statusMessage = "";
+
+}
